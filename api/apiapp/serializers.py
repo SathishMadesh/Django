b51dd4d70b0e42c3630a12bf463691550077aeca
SathishMadesh <s747272946@gmail.com>
2023-12-25 19:37:24 +0530

    update

--- api/apiapp/serializers.py
+++ api/apiapp/serializers.py
@@ -2,15 +2,25 @@ from rest_framework import serializers
 from .models import Product
 
 
-class ProductSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    image = serializers.URLField()
-    price = serializers.IntegerField()
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['title', 'image', 'price']
 
-    def create(self, validate_data):
-        return Product.objects.create(validate_data)
+
+
+
+
+
+
+#     title = serializers.CharField(max_length=100)
+#     image = serializers.URLField()
+#     price = serializers.IntegerField()
+
+#     def create(self, validate_data):
+#         return Product.objects.create(validate_data)
     
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', validated_data.title)
-        instance.image = validated_data.get('image', validated_data.image)
-        instance.price = validated_data.get('price', validated_data.price)
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', validated_data.title)
+#         instance.image = validated_data.get('image', validated_data.image)
+#         instance.price = validated_data.get('price', validated_data.price)
--- api/apiapp/views.py
+++ api/apiapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
+
+def ProductView(request):
+    if request.method == 'GET':
+        product = Product.objects.all()
+        serializer = ProductSerializer(product, many=True)
+        return JsonResponse(serializer,data, safe=False)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ProductSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data,status=201)
+        return JsonResponse(serializer.error, status=400)
