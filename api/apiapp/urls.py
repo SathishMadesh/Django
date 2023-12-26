d11c065c301fec86e42e6f629da74e9fe26488fd
SathishMadesh <s747272946@gmail.com>
2023-12-26 16:09:20 +0530

    update

--- api/apiapp/urls.py
+++ api/apiapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductsView
+from .views import ProductsView, product_details
 
 
 urlpatterns = [
-    path('product/', ProductsView)
+    path('products/', ProductsView),
+    path('product/<int:pk>', product_details)
 ]
--- api/apiapp/views.py
+++ api/apiapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Products
 from .serializers import ProductsSerializer
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 
@@ -20,10 +20,29 @@ def ProductsView(request):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data,status=201)
-        return JsonResponse(serializer.error, status=400)
+        return JsonResponse(serializer.errors, status=400)
 
 
-# @csrf_exempt
-# def product_details(requist, id):  # id or pk(primary key)
-#     try:
-#         product
+@csrf_exempt
+
+def product_details(request, pk):  # id or pk(primary key)
+    try:
+        product = Products.objects.get(pk=pk)
+    except product.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = ProductsSerializer(product)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ProductsSerializer(product, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    
+    elif request.method == 'DELETE':
+        product.delete()
+        return HttpResponse(status=204)
